ad72967b3e14b4c883391886464ea5e12208b79f
Alena <pluwa@mail.ru>
2023-05-08 15:46:45 +0400

    junit4 changed

---
+++ src/test/CashBackHackServiceTest.java
@@ -0,0 +1,54 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashBackHackServiceTest {
+    CashBackHackService service = new CashBackHackService();
+
+    @Test
+    public void testRemainSum0() {
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
+
+    }
+    @Test
+    public void testRemainSum1000(){
+
+        //тут ошибка
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testRemainSum999(){
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainSum1001(){
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testRemainSum2000(){
+        int amount = 2000;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+
+}
