a15238b118fea7e7d118c46221a909f110968b78
Alena <pluwa@mail.ru>
2023-05-14 14:48:54 +0400

    test changed

--- src/test/java/CashBackHackServiceTest.java
+++ src/test/java/CashBackHackServiceTest.java
@@ -14,16 +14,7 @@ public class CashBackHackServiceTest {
         assertEquals(actual, expected);
 
     }
-    @Test
-    public void testRemainSum1000(){
 
-        //тут ошибка
-        int amount = 1000;
-
-        int actual = service.remain(amount);
-        int expected = 0;
-        assertEquals(actual, expected);
-    }
     @Test
     public void testRemainSum999(){
         int amount = 999;
