13135d3944c65e49d1c2074685689c95aee93128
Alena <pluwa@mail.ru>
2023-05-14 14:41:45 +0400

    some changes

--- src/test/java/CashBackHackServiceTest.java
+++ src/test/java/CashBackHackServiceTest.java
@@ -1,6 +1,6 @@
 
 
-//package ru.netology.service;
+// package ru.netology.service;
 
 //import org.junit.jupiter.api.Assertions;
 import org.testng.annotations.Test;
@@ -20,16 +20,7 @@ public class CashBackHackServiceTest {
         assertEquals(actual, expected);
 
     }
-//    @Test
-//    public void testRemainSum1000(){
-//
-//        //тут ошибка
-//        int amount = 1000;
-//
-//        int actual = service.remain(amount);
-//        int expected = 0;
-//        assertEquals(actual, expected);
-//    }
+
     @Test
     public void testRemainSum999(){
         int amount = 999;
