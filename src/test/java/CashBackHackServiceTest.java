b7cadb501b89ab7e08e4b3176b333b062fac7b6b
Alena <pluwa@mail.ru>
2023-05-15 15:17:38 +0400

    test returne

--- src/test/java/CashBackHackServiceTest.java
+++ src/test/java/CashBackHackServiceTest.java
@@ -16,7 +16,7 @@ public class CashBackHackServiceTest {
     }
 
     @Test
-    public void testRemainSum999(){
+    public void testRemainSum999() {
         int amount = 999;
 
         int actual = service.remain(amount);
@@ -25,15 +25,27 @@ public class CashBackHackServiceTest {
     }
 
     @Test
-    public void testRemainSum1001(){
+    public void testRemainSum1000() {
+
+        //тут ошибка
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainSum1001() {
         int amount = 1001;
 
         int actual = service.remain(amount);
         int expected = 999;
         assertEquals(actual, expected);
     }
+
     @Test
-    public void testRemainSum2000(){
+    public void testRemainSum2000() {
         int amount = 2000;
 
         int actual = service.remain(amount);
